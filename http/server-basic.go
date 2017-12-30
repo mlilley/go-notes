4754af931777250a6e2787ffd5adf1aae5cc2039
mike <michael.lilley@gmail.com>
2017-12-29 22:55:48 -0800

    http server, basic

---
+++ http/server-basic.go
@@ -0,0 +1,29 @@
+package main
+
+# Note:
+#  - cannot have multiple servers per process with this method
+#  - url matching behavior may be surprising:
+#    - "/" is a catch all for everything not matched by other handlers
+#    - "/x" matches "/x" and "/xyz"
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func getRootHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "I am root!")
+}
+
+func getNotesHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Notes!")
+}
+
+func main() {
+	http.HandleFunc("/", getRootHandler)
+	http.HandleFunc("/notes", getNotesHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("Unalbe to start server")
+	}
+}
